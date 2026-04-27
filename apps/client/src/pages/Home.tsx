4146dde312041f55f3d51308ba917dd79a54b2d8
Warren Liang <war.liang96@gmail.com>
2026-04-26 19:05:17 -0700

    changes: add onSubmit handler for create game refactor: update colors for buttons in tokens refactor: add game settting states

--- apps/client/src/pages/Home.tsx
+++ apps/client/src/pages/Home.tsx
@@ -2,24 +2,35 @@ import { useState } from 'react';
 
 import { socket } from '../socket';
 
+import type { GameSettings } from '@euchrenow/engine';
+
 type SelectOption = 'play' | 'create' | 'join';
 
 const Home = () => {
   const [roomId, setRoomId] = useState<string>('');
   const [selected, setSelected] = useState<SelectOption>('join');
 
-  const onCreateGame = () => {
-    socket.emit(
-      'lobby:create',
-      { playerName: 'Player1', settings: { stickTheDealer: true, goingAlone: true } },
-      (response) => {
-        if (response.success) {
-          setRoomId(response.roomId!);
-        } else {
-          console.error('Failed to create game: ', response.error);
-        }
-      },
-    );
+  const [gameSettings, setGameSettings] = useState<GameSettings>({
+    stickTheDealer: true,
+    goingAlone: true,
+  });
+
+  const onSettingsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = e.target;
+    setGameSettings((prev) => ({ ...prev, [name]: checked }));
+  };
+
+  const onCreateGame = (e: React.FormEvent) => {
+    e.preventDefault();
+    socket.emit('lobby:create', { playerName: 'Player1', settings: gameSettings }, (response) => {
+      if (response.success) {
+        console.log('Game created with ID: ', response.roomId);
+        console.log('Response: ', response);
+        setRoomId(response.roomId!);
+      } else {
+        console.error('Failed to create game: ', response.error);
+      }
+    });
   };
 
   const selectButtons: {
@@ -40,10 +51,10 @@ const Home = () => {
 
   return (
     <div className="h-full w-full flex justify-center">
-      <div className="flex mt-14 w-4xl h-7/10 border-2 border-border rounded-2xl">
+      <div className="flex mt-14 w-4xl h-7/10 border-2 border-border rounded-2xl overflow-hidden">
         {!roomId && (
           <div className="flex flex-col h-full w-20 border-r border-r-border">
-            {selectButtons.map((btn, index) => (
+            {selectButtons.map((btn) => (
               <button
                 key={btn.value}
                 aria-selected={selected === btn.value}
@@ -51,7 +62,7 @@ const Home = () => {
                   setSelected(btn.value);
                   btn.onClick();
                 }}
-                className={`cursor-pointer py-2 px-2 hover:text-primary hover:bg-gray-800 aria-selected:text-primary  aria-selected:bg-gray-800 ${btn.class || ''} ${index === 0 && 'rounded-tl-2xl'}`}
+                className={`cursor-pointer py-2 px-2 hover:text-primary hover:bg-gray-800 aria-selected:text-primary  aria-selected:bg-gray-800 ${btn.class || ''}`}
               >
                 {btn.label}
               </button>
@@ -66,21 +77,36 @@ const Home = () => {
           <div className="h-full w-full flex flex-col items-center p-4">
             <h3 className="text-2xl font-sembold">Select game options:</h3>
             <div className="flex flex-col h-full w-full p-4 justify-between items-center">
-              <form>
+              <form onSubmit={onCreateGame} className="flex flex-col h-full w-8/10 items-center">
                 <div className="flex flex-col gap-2">
                   <label>
-                    <input type="checkbox" className="mr-2" />
+                    <input
+                      type="checkbox"
+                      name="stickTheDealer"
+                      checked={gameSettings.stickTheDealer}
+                      onChange={onSettingsChange}
+                      className="mr-2"
+                    />
                     Stick the Dealer
                   </label>
                   <label>
-                    <input type="checkbox" className="mr-2" />
-                    Allow Going Alone
+                    <input
+                      type="checkbox"
+                      name="goingAlone"
+                      checked={gameSettings.goingAlone}
+                      onChange={onSettingsChange}
+                      className="mr-2"
+                    />
+                    Going Alone
                   </label>
                 </div>
+                <button
+                  type="submit"
+                  className="cursor-pointer bg-btn-primary hover:bg-btn-active py-1 px-2 rounded-lg mt-auto self-end"
+                >
+                  Create Game
+                </button>
               </form>
-              <button type="submit" className="bg-primary py-1 px-2 rounded-lg self-end">
-                Create Game
-              </button>
             </div>
           </div>
         )}
